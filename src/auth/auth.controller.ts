3b70eba39a3f7715d8be1ce72aa166c40a775175
jp-knj <kenji.tomita1996@gmail.com>
2022-07-05 22:01:47 +0900

    :sparkles: using auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AutoDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: any) {
-    console.log(dto);
+  signup(@Body() autoDto: AutoDto) {
     return this.authService.signup();
   }
 
